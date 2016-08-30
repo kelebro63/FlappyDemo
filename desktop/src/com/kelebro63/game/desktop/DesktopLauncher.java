96f96e8033d532ae1892a8007473c38629c4f925
Kelebro63 <kelebro63@gmail.com>
2016-08-30 11:13:51 +0300

    part1

---
+++ desktop/src/com/kelebro63/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kelebro63.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kelebro63.game.FlappyDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyDemo(), config);
+	}
+}

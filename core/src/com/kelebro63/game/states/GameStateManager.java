c9fedcf471ed753911d84566b128ec259bf81ea1
Kelebro63 <kelebro63@gmail.com>
2016-08-30 13:03:28 +0300

    part2

---
+++ core/src/com/kelebro63/game/states/GameStateManager.java
@@ -0,0 +1,39 @@
+package com.kelebro63.game.states;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import java.util.Stack;
+
+/**
+ * Created by Bistrov Alexey on 30.08.2016.
+ */
+public class GameStateManager {
+
+    private Stack<State> states;
+
+
+    public GameStateManager() {
+        states = new Stack<State>();
+    }
+
+    public void push(State state) {
+        states.push(state);
+    }
+
+    public void pop() {
+        states.pop().dispose();
+    }
+
+    public void set(State state) {
+        states.pop().dispose();
+        states.push(state);
+    }
+
+    public void update(float dt) {
+        states.peek().update(dt);
+    }
+
+    public void render(SpriteBatch sb) {
+        states.peek().render(sb);
+    }
+}
---
+++ core/src/com/kelebro63/game/states/MenuState.java
@@ -0,0 +1,44 @@
+package com.kelebro63.game.states;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.kelebro63.game.FlappyDemo;
+
+/**
+ * Created by Bistrov Alexey on 30.08.2016.
+ */
+public class MenuState extends State{
+
+    private Texture background;
+    private Texture playBtn;
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playbtn.png");
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background, 0, 0, FlappyDemo.WIDTH, FlappyDemo.HEIGHT);
+        sb.draw(playBtn, (FlappyDemo.WIDTH / 2) - (playBtn.getWidth() / 2), FlappyDemo.HEIGHT / 2);
+        sb.end();
+    }
+
+    @Override
+    public void dispose() {
+        background.dispose();
+        playBtn.dispose();
+    }
+}
